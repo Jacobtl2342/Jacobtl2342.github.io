be26dd87e20042298a0daf5fada46d4494c12f2b
Jacob Lopez <jacobtl2342@gmail.com>
2020-12-23 11:54:37 -0600

    Update
    Changes include:
    - Reworked layout
    - Responsive nav
    - Updated media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
+const menuBtn = document.querySelector('.navToggle');
+const sideNav = document.querySelector('.sideNav');
 
+menuBtn.addEventListener('click', myFunction);
+
+function myFunction(){
+    if(sideNav.className === 'sideNav inactive'){
+        sideNav.classList.remove('inactive');
+        sideNav.classList.add('active');
+    }else{
+        sideNav.classList.remove('active');
+        sideNav.classList.add('inactive')
+    }
+}
