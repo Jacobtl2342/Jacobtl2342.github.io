28229ba68174595f3b5498ebfab4082a1b658aa8
Jacob Lopez <jacoblopez.dev@gmail.com>
2020-12-24 10:51:49 -0600

    Mobile Nav Update
    - Fixed nav no longer overlaps section headers
    - Nav closes upon clicking a link
    - Various CSS changes to nav links

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const menuBtn = document.querySelector('.navToggle');
 const sideNav = document.querySelector('.sideNav');
+const navLinks = document.querySelector('.nav-links')
 
 menuBtn.addEventListener('click', myFunction);
+navLinks.addEventListener('click', myFunction2);
 
 function myFunction(){
     if(sideNav.className === 'sideNav inactive'){
@@ -10,5 +12,12 @@ function myFunction(){
     }else{
         sideNav.classList.remove('active');
         sideNav.classList.add('inactive')
+    }
+}
+
+function myFunction2(){
+    if(sideNav.className === 'sideNav active'){
+        sideNav.classList.remove('active');
+        sideNav.classList.add('inactive');
     }
 }
